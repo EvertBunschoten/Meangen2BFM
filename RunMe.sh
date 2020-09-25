a85657028526eb55d9f9d5c9273027eae7ea39e7
EvertBunschoten <evertbun@xs4all.nl>
2020-09-25 13:18:09 +0200

    Updated the README file according to the latest changes

--- RunMe.sh
+++ RunMe.sh
@@ -8,8 +8,8 @@ echo "*********** 3D BFM Design Tool **********\n"
 echo "****************************************\n\n"
 echo "Add Following in your .bashrc\n"
 echo "\texport M2BFM=$HOME/"
-echo "\tPATH=\$PATH:\$HOME/executables/"
-echo "\tPYTHONPATH=\$PYTHONPATH:\$HOME/executables/\n\n"
+echo "\tPATH=\$PATH:$HOME/executables/"
+echo "\tPYTHONPATH=\$PYTHONPATH:$HOME/executables/\n\n"
 echo "*************************************\n\n"
 
 PYTHON3=$(which python3)
