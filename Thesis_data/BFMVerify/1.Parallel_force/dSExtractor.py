07061396eabce7ad874216bb18a711c52271f836
EvertBunschoten <evertbun@xs4all.nl>
2020-10-05 10:13:11 +0200

    added thesis data

---
+++ Thesis_data/BFMVerify/1.Parallel_force/dSExtractor.py
@@ -0,0 +1,71 @@
+from paraview.simple import *
+import numpy as np
+
+T_in = 281.1614467
+P_in = 101478.5239
+
+BFM = LegacyVTKReader(FileNames=["BFMOUTPUT.vtk"])
+
+TdS = Calculator(Input=BFM)
+TdS.ResultArrayName='dS'
+TdS.Function = '(1004.703*ln(Temperature/'+str(T_in)+') - 287.15*ln(Pressure/'+str(P_in)+'))'
+RenameSource("dS", TdS)
+
+
+slice1 = Slice(Input = TdS)
+slice1.SliceType.Normal = [0, 0, 1]
+slice1.SliceType.Origin = [0, 0, -0.05]
+
+IV = IntegrateVariables(Input = slice1)
+
+zrange = np.linspace(-0.09, 0.15, 1000)
+
+outputFile = open("outputData.txt", "w+")
+outputFile.write("ax\tf_p\tTds_dax\n")
+ds = []
+P = []
+BF_z = []
+T = []
+rho = []
+u_x = []
+for z in zrange:
+    slice1.SliceType.Origin = [0, 0, z]
+    iv_data = paraview.servermanager.Fetch(IV)
+    area = iv_data.GetCellData().GetArray("Area").GetValue(0)
+    BF_z.append(iv_data.GetPointData().GetArray("Body-Force").GetValue(2)/area)
+    rho.append(iv_data.GetPointData().GetArray("Density").GetValue(0)/area)
+    u_x.append(iv_data.GetPointData().GetArray("Momentum").GetValue(2)/iv_data.GetPointData().GetArray("Density").GetValue(0))
+    ds.append(iv_data.GetPointData().GetArray("dS").GetValue(0)/area)
+    T.append(iv_data.GetPointData().GetArray("Temperature").GetValue(0)/area)
+    P.append(iv_data.GetPointData().GetArray("Pressure").GetValue(0)/area)
+    #outputFile.write(str(z)+"\t"+str(BF_z)+"\t"+str(tds)+"\n")
+  
+
+Tdsdz = []
+dpdz = []
+for i in range(len(zrange)):
+    if i ==0:
+        deltaZ = zrange[i+1] - zrange[i]
+        deltaS = ds[i+1] - ds[i]
+        deltaP = P[i+1] - P[i]
+        deltaMom = rho[i+1]*u_x[i+1]**2 -  rho[i]*u_x[i]**2
+    elif i == len(zrange)-1:
+        deltaZ = zrange[i] - zrange[i-1]
+        deltaS = ds[i] - ds[i-1]
+        deltaP = P[i] - P[i-1]
+        deltaMom = rho[i]*u_x[i]**2 -  rho[i-1]*u_x[i-1]**2
+    else:
+        deltaZ = zrange[i+1] - zrange[i-1]
+        deltaS = ds[i+1] - ds[i-1]
+        deltaP = P[i+1] - P[i-1]
+        deltaMom = rho[i+1]*u_x[i+1]**2 -  rho[i-1]*u_x[i-1]**2
+     
+    dS_dZ = deltaS/deltaZ
+    dP_dZ = deltaP / deltaZ
+    dMom_dz = deltaMom / deltaZ
+    TdS_dZ = T[i]*dS_dZ
+    Tdsdz.append(TdS_dZ)
+    dpdz.append(dP_dZ)
+    outputFile.write(str(zrange[i])+"\t"+str(rho[i])+'\t'+str(-BF_z[i]/rho[i])+"\t"+str(TdS_dZ)+"\t"+str(dP_dZ)+"\t"+str(dMom_dz)+"\n")
+        
+outputFile.close()
---
+++ Thesis_data/BFMVerify/2.Blockage_model/dataPlotter.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import os
+
+fsize = 15
+ticksize = 13
+P_t_in = 105325
+T_t_in = 288.7
+fracfactor = 1.3
+tickformat='%.2f'
+figsize=(6, 5)
+
+save_direct = os.getcwd()+'Images/'
+
+z_blade, p_blade, T_blade, pt_blade, Tt_blade, ds_blade, alpha_blade, Mach_blade, flux_blade = np.loadtxt(save_direct+"../axialData_blade.txt", unpack=True, delimiter='\t', skiprows=1)
+z_bfm, p_bfm, T_bfm, pt_bfm, Tt_bfm, ds_bfm, alpha_bfm, Mach_bfm, flux_bfm = np.loadtxt(save_direct+"../axialData_BFM.txt", unpack=True, delimiter='\t', skiprows=1)
+
+
+fig = plt.figure(0, figsize=figsize)
+ax = fig.add_subplot(111)
+plt.title("Mass flow averaged pressure plot", fontsize=fsize)
+ax.plot(z_blade, p_blade/P_t_in, 'r', label="Blade computation")
+ax.plot(z_bfm, p_bfm/P_t_in, 'b', label="BFM")
+ax.plot(z_blade, 1.01*p_blade/P_t_in, 'k--', label="+- 1%")
+ax.plot(z_blade, 0.99*p_blade/P_t_in, 'k--')
+plt.xlabel("x[m]", fontsize=fsize)
+plt.ylabel(r"$\frac{p}{p_{t, in}}$", fontsize=fracfactor*fsize)
+ax.legend(fontsize=fsize, bbox_to_anchor=(1.04, 1), loc="upper left")
+ax.grid()
+ax.tick_params(axis='both', which='both', labelsize=ticksize)
+ax.xaxis.set_major_formatter(FormatStrFormatter(tickformat))
+ax.yaxis.set_major_formatter(FormatStrFormatter(tickformat))
+fig.savefig(save_direct + 'P_stat.eps', bbox_inches='tight', format='eps')
+
+fig = plt.figure(1, figsize=figsize)
+ax = fig.add_subplot(111)
+plt.title("Mass flow averaged Mach plot", fontsize=fsize)
+ax.plot(z_blade, Mach_blade, 'r', label="Blade computation")
+ax.plot(z_bfm, Mach_bfm, 'b', label="BFM")
+ax.plot(z_blade, 1.01*Mach_blade, 'k--', label="+- 1%")
+ax.plot(z_blade, 0.99*Mach_blade, 'k--')
+plt.xlabel("x[m]", fontsize=fsize)
+plt.ylabel(r"$M$", fontsize=fsize)
+ax.legend(fontsize=fsize, bbox_to_anchor=(1.04, 1), loc="upper left")
+ax.grid()
+ax.tick_params(axis='both', which='both', labelsize=ticksize)
+ax.xaxis.set_major_formatter(FormatStrFormatter(tickformat))
+ax.yaxis.set_major_formatter(FormatStrFormatter(tickformat))
+fig.savefig(save_direct + 'Mach.eps', bbox_inches='tight', format='eps')
+
+
+fig = plt.figure(2, figsize=figsize)
+ax = fig.add_subplot(111)
+plt.title("Mass flux plot", fontsize=fsize)
+ax.plot(z_blade, flux_blade, 'r', label="Blade computation")
+ax.plot(z_bfm, flux_bfm, 'b', label="BFM")
+ax.plot(z_blade, 1.01*flux_blade, 'k--', label="+- 1%")
+ax.plot(z_blade, 0.99*flux_blade, 'k--')
+plt.xlabel("x[m]", fontsize=fsize)
+plt.ylabel(r"$\rho u_x[kg m^{-2}s^{-1}]$", fontsize=fsize)
+ax.legend(fontsize=fsize, bbox_to_anchor=(1.04, 1), loc="upper left")
+ax.grid()
+ax.tick_params(axis='both', which='both', labelsize=ticksize)
+ax.xaxis.set_major_formatter(FormatStrFormatter(tickformat))
+ax.yaxis.set_major_formatter(FormatStrFormatter(tickformat))
+fig.savefig(save_direct + 'flux.eps', bbox_inches='tight', format='eps')
+
+plt.show()
